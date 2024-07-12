52151ac3a1436fea7315738915aaef2834209a58
Clover <2621869236@qq.com>
2024-07-12 15:17:38 +0800

    fix: turbo start error

--- src/app/main/chat/page.tsx
+++ src/app/main/chat/page.tsx
@@ -1,16 +1,16 @@
-'use client'
-
 import { ChatRoom } from "@/components/ChatRoom"
-import { ChatList } from "@/components/list/chat"
 import { WindowDisplayController } from '@/components/helper/WindowDisplayController'
+import { ChatList } from "@/components/list/chat"
 
 function Chat() {
   return (
     <div className="chat flex h-screen">
       <WindowDisplayController />
+
       <div className="flex-shrink-0 border-r-[var(--border-color)] border-r-solid border-r-1 diable-select-text">
         <ChatList />
       </div>
+
       <div className="flex-grow overflow-hidden">
         <ChatRoom />
       </div>
