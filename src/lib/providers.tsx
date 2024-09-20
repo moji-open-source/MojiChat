0232fbab0f781f1cdcfe8c72e405eecde70995d4
Clover You <2621869236@qq.com>
2024-09-20 09:08:05 +0800

    feat: disable context menu

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -4,16 +4,25 @@ import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider } from 'next-themes'
 import { useEffect } from 'react'
 
+function setup() {
+  if (window) {
+    window.addEventListener('contextmenu', (e) => {
+      if (process.env.NODE_ENV === 'production')
+        e.preventDefault()
+    })
+  }
+}
+
 export function Providers({ children }: { children: React.ReactNode }) {
   function onKeydownListener(event: KeyboardEvent) {
     if (event.metaKey) {
       if (event.key === '=' || event.key === '-')
         event.preventDefault()
-      // document.body.style.zoom = zoom.current += event.key === '=' ? 0.1 : -0.1
     }
   }
 
   useEffect(() => {
+    setup()
     window.addEventListener('keydown', onKeydownListener)
     return () => {
       window.removeEventListener('keydown', onKeydownListener)
